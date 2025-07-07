8bd2a7e53c942d6633c103271696b2e48a1e3615
linuxul <jsseo@eflash.co.kr>
2025-07-07 13:05:07 +0900

    feat: h2database 처리

--- sample/sample034/src/main/java/com/example/sample034/Member.java
+++ sample/sample034/src/main/java/com/example/sample034/Member.java
@@ -2,6 +2,7 @@ package com.example.sample034;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -15,7 +16,7 @@ import lombok.NoArgsConstructor;
 public class Member {
 
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String name;
