e701700a3ffa6298d8bea98e4a7134844406892e
linuxul <jsseo@eflash.co.kr>
2025-07-18 04:55:21 +0900

    feat: 사용하지 않는 코드 삭제

--- sample/sample040/src/main/java/com/youtube/project/sample040/controller/MemberController.java
+++ sample/sample040/src/main/java/com/youtube/project/sample040/controller/MemberController.java
@@ -79,50 +79,5 @@ public class MemberController {
                 .getRequestDispatcher("/api/articles?memberId=" + id)
                 .forward(request, response);
     }
-
-//
-//    @PostMapping
-//    public Member save(@RequestBody Member member) {
-//        return memberRepository.save(member);
-//    }
-//
-//    @GetMapping
-//    public List<Member> findAll() {
-//        return memberRepository.findAll();
-//    }
-//
-//    @GetMapping("/{id}")
-//    public Member findById(@PathVariable Long id) {
-//        return memberRepository.findById(id).orElse(null);
-//    }
-//
-//    @PutMapping("/{id}")
-//    public Member update(@PathVariable Long id, @RequestBody Member member) {
-//        member.setId(id);
-//        return memberRepository.save(member);
-//    }
-//
-//    @PatchMapping("/{id}")
-//    public Member patch(@PathVariable Long id, @RequestBody Member patch) {
-//        Member member = memberRepository.findById(id).orElse(null);
-//        if (member != null) {
-//            if (patch.getName() != null) {
-//                member.setName(patch.getName());
-//            }
-//            if (patch.getEmail() != null) {
-//                member.setEmail(patch.getEmail());
-//            }
-//            if (patch.getAge() != null) {
-//                member.setAge(patch.getAge());
-//            }
-//            memberRepository.save(member);
-//        }
-//        return member;
-//    }
-//
-//    @DeleteMapping("/{id}")
-//    public void delete(@PathVariable Long id) {
-//        memberRepository.deleteById(id);
-//    }
-
+    
 }
